dbdad44eba4fba9732e35c580f964a10c380a930
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2022-04-12 17:30:53 -0300

    fix: Update per item metrics description to match metrics.

--- src/exporter/metrics.py
+++ src/exporter/metrics.py
@@ -9,19 +9,19 @@ def get_metrics():
     total_managed = Gauge('total_managed', 'Number of resources fully managed by Iac.')
     coverage = Gauge('coverage', 'Relation between `total_managed` and `total_resources`.')
     managed_items_count_by_type = Gauge('managed_items_count_by_type',
-                                        'Number of resources by type fully managed by Iac. '
+                                        'Resources by type fully managed by Iac. '
                                         'Eg: number of `aws_s3_bucket` and so on.',
                                         labelnames=['resource'])
     unmanaged_items_count_by_type = Gauge('unmanaged_items_count_by_type',
-                                          'Number of resources by type fully managed by Iac. '
+                                          'Resources present on cloud provider but no declared on Iac.'
                                           'Eg: number of `aws_iam_user` and so on.',
                                           labelnames=['resource'])
     missing_items_count_by_type = Gauge('missing_items_count_by_type',
-                                        'Number of resources by type fully managed by Iac. '
+                                        'Resources declared on Iac but missing from the cloud provider.'
                                         'Eg: number of `aws_sqs_queue` and so on.',
                                         labelnames=['resource'])
     changed_items_count_by_type = Gauge('changed_items_count_by_type',
-                                        'Number of resources that are declared on Iac but were changed externally, '
+                                        'Resources that are declared on Iac but were changed externally.'
                                         'by resource type. Eg: number of `aws_dynamodb_table` and so on.',
                                         labelnames=['resource'])
 
