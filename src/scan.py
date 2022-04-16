9c6cd83c6830f10d75ec2c0929c691a2b3218291
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2022-04-16 09:41:50 -0300

    chore: Added minimal scan tests.

--- src/scan.py
+++ src/scan.py
@@ -1,7 +1,7 @@
 import os
 
 from exporter.repositories import DriftScanCmdRepository, SmartyResultRepositoryFactory
-from exporter.usecases import scan_and_save_drift_on_s3_usecase
+from exporter.usecases import scan_and_save_drift_on_storage_usecase
 from exporter.domain import Rfc1808Url
 
 DCTL_FROM = os.environ["DCTL_FROM"]
@@ -13,4 +13,5 @@ url = Rfc1808Url.from_url(RESULT_PATH)
 drift_repository = DriftScanCmdRepository()
 result_repo = SmartyResultRepositoryFactory.get_repository_by_scheme_url(url)
 
-scan_and_save_drift_on_s3_usecase(drift_repository, result_repo)
+if __name__ == '__main__':
+    scan_and_save_drift_on_storage_usecase(drift_repository, result_repo)
